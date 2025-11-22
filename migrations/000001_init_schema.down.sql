195038712fe35915b5fcce05b3f120a08e7c367b
Konstantin Kuznetsov <KyzN206@yandex.ru>
2025-11-22 11:09:58 +0300

    применена миграция базы данных

---
+++ migrations/000001_init_schema.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS pr_reviewers;
+DROP TABLE IF EXISTS pull_requests;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS teams;
+
+DROP INDEX IF EXISTS idx_pr_reviewers_user;
+DROP INDEX IF EXISTS idx_pr_author;
+DROP INDEX IF EXISTS idx_users_team;
