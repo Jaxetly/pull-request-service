4c43cc774e53cee7c7629b590073c915069bf2dd
Konstantin Kuznetsov <KyzN206@yandex.ru>
2025-11-23 20:54:13 +0300

    более строгие сообщения ошибок

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -4,11 +4,11 @@ import "errors"
 
 var (
 	ErrTeamNotFound        = errors.New("team not found")
-	ErrTeamExists          = errors.New("team already exists")
 	ErrUserNotFound        = errors.New("user not found")
-	ErrPRNotFound          = errors.New("pull request not found")
-	ErrPRExists            = errors.New("pull request already exists")
-	ErrPRMerged            = errors.New("pull request is already merged")
-	ErrNoCandidate         = errors.New("there are no available candidates for this pull request")
-	ErrReviewerNotAssigned = errors.New("user is not assigned as reviewer for this pull request")
+	ErrPRNotFound          = errors.New("PR not found")
+	ErrTeamExists          = errors.New("team_name already exists")
+	ErrPRExists            = errors.New("PR id already exists")
+	ErrPRMerged            = errors.New("cannot reassign on merged PR")
+	ErrNoCandidate         = errors.New("no active replacement candidate in team")
+	ErrReviewerNotAssigned = errors.New("reviewer is not assigned to this PR")
 )
